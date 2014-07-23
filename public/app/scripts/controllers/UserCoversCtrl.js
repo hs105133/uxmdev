0c4ee99b4b9073ad88d9e593988f44dc1cdf4516
Hemant <hemant.singh@techmahindra.com>
2014-07-24 00:04:38 +0530

    manage cover delete issue fix

--- public/app/scripts/controllers/UserCoversCtrl.js
+++ public/app/scripts/controllers/UserCoversCtrl.js
@@ -2,7 +2,9 @@
 angular.module('techmApp').controller('UserCoversCtrl', function($scope, UserService, $rootScope) {     
     $scope.myCovers = UserService.covers().query({userId: $rootScope.currentUser.id});
     $scope.deleteCover = function(index, coverId){
+    	console.log(index, coverId);
         UserService.covers().remove({id: coverId});
         $scope.myCovers.splice(index,1);
+        console.log($scope.myCovers);
     };
 });
