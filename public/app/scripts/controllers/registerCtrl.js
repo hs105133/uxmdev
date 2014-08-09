f96a5707848f5efb9947c717f3f8d83bb02d546b
Hemant <hemant.singh@techmahindra.com>
2014-08-09 22:07:19 +0530

    admin user create+ redirect

--- public/app/scripts/controllers/registerCtrl.js
+++ public/app/scripts/controllers/registerCtrl.js
@@ -11,19 +11,29 @@ angular.module('techmApp').controller('RegisterCtrl', function($scope, $rootScop
 
     $scope.register = function() {
         $scope.$parent.flashMsg = false;
-        if($location.path() === '/add-admin'){
-             $scope.formData.role = "Admin";
+        if ($location.path() === '/add-admin') {
+            $scope.formData.role = "Admin";
         }
         UserService.user().save($scope.formData, function(userData) {
 
-            UserService.logIn($scope.formData).success(function(res, error) {
-                UserService.getMe(userData);
-                $location.path("/");
+            UserService.logIn($scope.formData).success(function(res, error) {   
+                if ($location.path() === '/add-admin') {
+                    $scope.alertClass = "alert-success"; 
+                    $scope.$parent.flashMsg = "Admin User created successfully";
+                    $scope.formData = {};
+                    $scope.signupForm.$setPristine();
+                    signupForm.reset(); // jshint ignore:line
+                } else {
+                    UserService.getMe(userData);
+                    $location.path("/");                    
+                }
             }, function(res) {
+                $scope.alertClass = "alert-danger";
                 $scope.$parent.flashMsg = res.data.message;
             });
 
         }, function(res) {
+            $scope.alertClass = "alert-danger";
             $scope.$parent.flashMsg = res.data.errors ? "Username is already in use" : res.data.message;
         });
 
